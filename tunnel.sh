94168d35ed927655b91cf1f5480088cceb96afc0
flxxyz <i@mdzz.name>
2021-11-09 18:57:22 +0800

    判断操作系统

--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-echo 'tunnel'
+VERSION="0.0.1"
+
+if [ "$(uname)" == "Darwin" ];then
+  echo "macos"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
+  echo "linux"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ];then
+  echo "windows"
+fi
